1d0771701ae0053b108ba7ce53ffd9880a9209da
victor araujo <email.victornascimento@gmail.com>
2026-05-18 17:03:45 -0300

    feat: estrutura inicial da todo list e readme

---
+++ TodoApp/src/components/TodoApp.jsx
@@ -0,0 +1,62 @@
+import { useState } from "react";
+import './TodoApp.css'
+
+const TodoApp = () => {
+
+    const [todos, setTodos] = useState([]);
+
+    const [inputValue, setInputValue] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if(inputValue.trim() !== '') {
+            const newTodo = {
+                id: Date.now(),
+                Text:inputValue
+            }
+
+            setTodos((prevTodos) => [...prevTodos, newTodo])
+            setInputValue('');
+        }
+
+
+    }
+
+    const handleDelete = (id) => {
+        const updateTodos = todos.filter(todo => todo.id !== id);
+        setTodos(updateTodos);
+    }
+
+
+    return(
+        <div className="container">
+            <h1 className="title">TaskCraft</h1>
+            <form onSubmit={handleSubmit} className="todo-form">
+                <input 
+                    type="text" 
+                    placeholder="Adicione uma tarefa" 
+                    value={inputValue} 
+                    onChange={(e) => setInputValue(e.target.value)}
+                />
+                <button type="submit" className="add-button">
+                    Adicionar
+                </button>
+            </form>
+            <div className="container2">
+            </div>
+            {todos.length === 0 && <p className="empty"> não há tarefas</p>}
+
+            <ul className="todo-list">
+                {todos.map((todo) => (
+                    <li key={todo.id} className="todo-item">
+                        {todo.Text}
+                        <button className="delete-button" onClick={() => handleDelete(todo.id)}>Excluir</button>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+export default TodoApp;
